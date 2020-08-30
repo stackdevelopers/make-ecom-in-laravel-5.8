b01dccc9a09f124c03ecdfba3879ccd24fff62d6
Stack Developers <expertphpdevelopers@gmail.com>
2020-08-30 15:09:00 +0530

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,8 +1,8 @@
--- To get the Database, Please join Stack Developers Youtube Channel with below link :-
+-- To get the Database, Please join Stack Developers Youtube Channel as Premium Member with below link :-
 -- https://www.youtube.com/channel/UCExO2i-tLU1NyVZD6zOJQlw/join
 
--- Free Steps to get Database :-
--- Steps to download Basic E-commerce Website in Laravel 5.6/5.7/5.8 from Part 1 to 150 that includes the admin panel for E-commerce website involving categories, products, attributes and images modules  :-
+-- Steps to download Database after becoming Premium Member :-
+-- Steps to download Basic E-commerce Website in Laravel 5.6/5.7/5.8 from Part 1 to 150 along with database  :-
 
 /*
 
@@ -11,36 +11,12 @@ http://sitemakers.in
 
 2) Click on "STACK DEVELOPERS" link at top navigation that will take you to Stack Developers section
 
-3) Click on "Login to Download" link to download Basic E-commerce Series in Laravel 6/7 (Part 1 to 150)
+3) Click on "Login to Download" link to download Basic E-commerce Series in Laravel 5.6/5.7/5.8 (Part 1 to 150)
 
-4) Enter Username and Password if you have already got it otherwise get one by taking below steps :-
-
-Basic and Advance Series code will be given to the regular users who can do below :-
-
-Free Steps :-
-
-4.1) Subscribe Stack Developers Youtube Channel (if not subscribed yet) :-
-https://www.youtube.com/stackdevelopers
-
-4.2) Join Stack Developers Facebook Group :- https://www.facebook.com/groups/stackdevelopers
-
-4.3) Like Stack Developers Facebook Page :- https://www.facebook.com/stacdevelopers
-
-4.4) Like and Comment on every video of Advance E-com Series seen/made so far (in case following advance series)
-https://www.youtube.com/playlist?list=PLLUtELdNs2ZaHaFmydqjcQ-YyeQ19Cd6u
-
-Like and Comment on every video of Basic E-com Series seen/made so far (in case following basic series)
-https://www.youtube.com/playlist?list=PLLUtELdNs2ZY5drPxIWzpq5crhantlzp7
-
-(Username/Password will be emailed to you or shared on Social network)
-
-Or
-
-Paid Step :-
-
-4.1) Simply become channel member :-
+4) Enter Username and Password if you have already got it otherwise get one by joining Stack Developers Youtube Channel as Premium Member with below link :-
 https://www.youtube.com/channel/UCExO2i-tLU1NyVZD6zOJQlw/join
-(Username/Password will be emailed to you or shared on Social network)
+
+(Username/Password will be emailed to you / shared at Social network / Channel Community only for Premium Members)
 
 5) After login, click on "Click Here to Download" link to download Basic E-commerce Series in Laravel 5.6/5.7/5.8 (Part 1 to 150).
 
